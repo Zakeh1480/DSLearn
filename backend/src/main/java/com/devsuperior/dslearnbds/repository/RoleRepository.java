9041948bbffe74a07d58176ccab55443bc3fbdc6
Zakeh1480 <zakeh0875@hotmail.com>
2022-08-18 19:41:37 -0300

    Repositories

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.devsuperior.dslearnbds.repository;
+
+import com.devsuperior.dslearnbds.entities.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
---
+++ backend/src/main/java/com/devsuperior/dslearnbds/repository/SectionRepository.java
@@ -0,0 +1,7 @@
+package com.devsuperior.dslearnbds.repository;
+
+import com.devsuperior.dslearnbds.entities.Section;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SectionRepository extends JpaRepository<Section, Long> {
+}
